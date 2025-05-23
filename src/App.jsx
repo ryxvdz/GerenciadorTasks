15214072eed6563a7c70498a60d012d1381f8bbc
Ryan Dias <ryanvicd2112@gmail.com>
2025-05-23 15:20:22 -0300

    atualizando erro

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ function App() {
   return (
     <div className="min-h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
-        <Title>Genrenciador de Tarefa</Title>
+        <Title>Genrenciador de Tarefas</Title>
         <AddTasks onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks
           tasks={tasks}
